9e26cf22b4812fa92a16624e0895e3a2c880509f
ars <xakars02@gmail.com>
2023-05-04 11:42:45 +0600

    improve getting quiz

--- open_quiz.py
+++ open_quiz.py
@@ -1,22 +1,27 @@
 import os
 import random
 import re
+import argparse
 
 
-path_to_quiz = 'quiz-questions'
-quiz_file_names =  os.listdir(path_to_quiz)
-path_to_rand_quiz_file = os.path.join(path_to_quiz, random.choice(quiz_file_names))
+def get_rand_quiz(path):
+    questions = {}
+    with open(path, 'r', encoding='KOI8-R') as f:
+        quiz = f.read()
+        splitted_text = quiz.split('\n\n')
+        for count, string in enumerate(splitted_text):
+            if 'Вопрос ' in string:
+                question_text = re.sub(r'Вопрос \d+:', '', string).strip()
+                answer_text = re.sub(r'Ответ:', '', splitted_text[count + 1]).strip()
+                questions[question_text] = answer_text
 
-questions = {}
+    return questions
 
-with open(path_to_rand_quiz_file, 'r', encoding='KOI8-R') as f:
-    quiz = f.read()
-    splitted_text = quiz.split('\n\n')
-    for count, string in enumerate(splitted_text):
-        if 'Вопрос ' in string:
-            question_text = re.sub(r'Вопрос \d+:', '', string).strip()
-            answer_text = re.sub(r'Ответ:', '', splitted_text[count + 1]).strip()
-            questions[question_text] = answer_text
-
-print(questions)
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default='quiz-questions', help='path to quizzes files')
+    args = parser.parse_args()
+    path_to_quizzes = args.path
+    quizzes_file_names = os.listdir(path_to_quizzes)
+    path_to_rand_quiz_file = os.path.join(path_to_quizzes, random.choice(quizzes_file_names))
+    get_rand_quiz(path_to_rand_quiz_file)
